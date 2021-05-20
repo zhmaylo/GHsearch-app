a82684730453d98f9b3f5552467292cd6f41df64
zhmaylo <dzhmaylo@gmail.com>
2021-05-20 16:23:36 +0300

    202105201620

--- src/api/search/search.tsx
+++ src/api/search/search.tsx
@@ -4,37 +4,23 @@ import { URL_GET } from "../../constants/urlConst";
 import { clone } from "../clone";
 import { fetchData } from "../fetch/fetchData";
 
+// get SearchResult - return search result
 // request - search string
+// return outProducts - array
 export const getSearchResult = (request: string) => {
     console.log('🚀 ~ file: search.tsx ~ line 9 ~ getSearchResult ~ request', request);
     let outProducts = [];
     // scheck search length
     if (request.length == 0) return null;
 
-    // let regExpArr = getRegExpArr(request);
     // console.log('🚀 ~ file: search.tsx ~ line 14 ~ getSearchResult ~ regExpArr', regExpArr);
     getFetch(request);
-
     return outProducts;
 };
 
 
-// getRegExpArr - return regular expression array
-// request - search string
-// return - array of regular expression 
-const getRegExpArr = (request: string) => {
-    // query split into substrings
-    let requestArr = request.match(/[a-zа-я0-9]+/gui);
-    // array of regular expression 
-    let regExpArr = new Array();
-    requestArr.forEach((item: any) => {
-        let regEx = new RegExp(item, 'iu');
-        regExpArr.push(regEx);
-    });
-    return regExpArr;
-};
-
-
+// gerFetch - get data from server
+// request - search requst
 const getFetch = async (request: any,) => {
     let i = 0;
     let data1 = [];
@@ -42,9 +28,9 @@ const getFetch = async (request: any,) => {
     let outdata = [];
     while (i < URL_GET.length) {
         fetchData(URL_GET[i] + request).then(data => {
-            console.log('🚀 ~ file: search.tsx ~ line 45 ~ getFetch ~ data1', data1);
-            outdata = outdata.concat(data1);
-            console.log('🚀 ~ file: search.tsx ~ line 41 ~ getFetch ~ outdata', outdata);
+            console.log('🚀 ~ file: search.tsx ~ line 31 ~ getFetch ~ data', data);
+            outdata = outdata.concat(data);
+            console.log('🚀 ~ file: search.tsx ~ line 33 ~ getFetch ~ outdata', outdata);
             i++;
         });
     }
